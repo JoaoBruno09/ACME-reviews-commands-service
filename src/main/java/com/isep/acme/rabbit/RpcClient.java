f2b4ea142fde67ff2fd925c93b76829cd2d8c222
Tiago Lacerda <1220285@isep.ipp.pt>
2023-04-15 17:17:35 +0100

    added rpc getVotes

--- src/main/java/com/isep/acme/rabbit/RpcClient.java
+++ src/main/java/com/isep/acme/rabbit/RpcClient.java
@@ -5,11 +5,15 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.isep.acme.model.Product;
 import com.isep.acme.model.Review;
 import com.isep.acme.model.User;
+import com.isep.acme.model.Vote;
 import com.isep.acme.model.dtos.ReviewDTO;
+import com.isep.acme.model.dtos.VoteReviewDTO;
 import com.isep.acme.model.mappers.ReviewMapper;
+import com.isep.acme.model.mappers.VoteMapper;
 import com.isep.acme.repositories.ProductRepository;
 import com.isep.acme.repositories.ReviewRepository;
 import com.isep.acme.repositories.UserRepository;
+import com.isep.acme.repositories.VoteRepository;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -32,7 +36,11 @@ public class RpcClient {
     private ReviewRepository reviewRepository;
 
     @Autowired
+    private VoteRepository voteRepository;
+    @Autowired
     UserRepository uRepository;
+
+    private static final VoteMapper VOTE_MAPPER = VoteMapper.INSTANCE;
     public RpcClient(){
 
     }
@@ -91,6 +99,36 @@ public class RpcClient {
         }
     }
     public void getVotes() throws Exception{
+        String correlationId = UUID.randomUUID().toString();
+
+        MessageProperties props = new MessageProperties();
+        props.setReplyTo("#{queueRPC.name}");
+        props.setCorrelationId(correlationId);
+
+        String message = "getVotes";
+        Message requestMessage = new Message(message.getBytes(), props);
+        ArrayList<VoteReviewDTO> votesList = (ArrayList<VoteReviewDTO>) rabbitTemplate.convertSendAndReceive("rpc_votes_queue", requestMessage);
+
+
+        System.out.println(votesList);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        for (int i = 0; i < votesList.size(); i++) {
+            System.out.println(votesList.get(i));
+            VoteReviewDTO voteReviewDTO = objectMapper.convertValue(votesList.get(i), VoteReviewDTO.class);
+            System.out.println("Review RID " + voteReviewDTO.getVID());
+            Optional<Review> review = reviewRepository.findByRID(voteReviewDTO.getRID());
+            if (!review.isEmpty()){
+                Vote vote = VOTE_MAPPER.toVote(voteReviewDTO);
+                if (vote.getVote().equalsIgnoreCase("upVote") && review.get().addUpVote(vote)
+                        || vote.getVote().equalsIgnoreCase("downVote") && review.get().addDownVote(vote)) {
+                    voteRepository.save(vote);
+                    reviewRepository.save(review.get());
+                    System.out.println("Vote Added " + vote);
+                }
+            }
+        }
 
     }
     private Review getProductAndUserForReview(ReviewDTO reviewDTO){
@@ -101,4 +139,5 @@ public class RpcClient {
         review.setProduct(product.get());
         return review;
     }
+
 }
