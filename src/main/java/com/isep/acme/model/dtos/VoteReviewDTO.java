889cfbb1a7082d5ce7a1bb6ec4e2abcfbdde7941
João Rocha <1220256@isep.ipp.pt>
2023-04-15 16:47:03 +0100

    Made Reviews Commands Service to receive voteDTO and insert vote in its databse

---
+++ src/main/java/com/isep/acme/model/dtos/VoteReviewDTO.java
@@ -0,0 +1,18 @@
+package com.isep.acme.model.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class VoteReviewDTO {
+    private Long voteID;
+    private String VID;
+    private String vote;
+    private String RID;
+    private Long userID;
+}
